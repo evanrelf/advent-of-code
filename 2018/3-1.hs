4a6288de837ca5f2b2cf26f1f3154cb2421ada1a
Evan Relf <evan@evanrelf.com>
2019-12-01 14:33:28 -0800

    Complete 2018 day 3 part 1

--- 2018/3-1.hs
+++ 2018/3-1.hs
@@ -2,72 +2,55 @@
 
 import Prelude hiding (id)
 
-import Debug.Trace
-
 import Data.Function ((&))
-import Text.ParserCombinators.ReadP
+import Text.ParserCombinators.ReadP (char, munch1, readP_to_S, string)
+
+data Claim = Claim
+  { _id :: Int
+  , _x :: Int
+  , _y :: Int
+  , _width :: Int
+  , _height :: Int
+  } deriving Show
 
 type Fabric = [[Int]]
 
-data Claim = Claim
-  { id :: Int
-  , x :: Int
-  , y :: Int
-  , w :: Int
-  , h :: Int
-  } deriving Show
+parseClaim :: String -> Claim
+parseClaim = fst . head . readP_to_S
+  (Claim
+    <$> (char '#' *> int)
+    <*> (string " @ " *> int)
+    <*> (char ',' *> int)
+    <*> (string ": " *> int)
+    <*> (char 'x' *> int))
+  where int = read <$> munch1 (`elem` ['0'..'9'])
 
-digit :: ReadP Char
-digit = satisfy (`elem` ['0'..'9'])
-
-int :: ReadP Int
-int = read <$> many1 digit
-
-claim :: ReadP Claim
-claim = do
-  _ <- char '#'
-  id <- int
-  _ <- string " @ "
-  x <- int
-  _ <- char ','
-  y <- int
-  _ <- string ": "
-  w <- int
-  _ <- char 'x'
-  h <- int
-  pure Claim { id, x, y, w, h }
-
-parse :: String -> Claim
-parse = fst . head . readP_to_S claim
-
-initialFabric :: Fabric
-initialFabric = replicate 1000 (replicate 1000 0)
+parse :: String -> [Claim]
+parse = fmap parseClaim . lines
 
 modifyAt :: (a -> a) -> Int -> [a] -> [a]
 modifyAt f i xs = take i xs <> [f (xs !! i)] <> drop (i + 1) xs
 
 modifyAt2d :: (a -> a) -> Int -> Int -> [[a]] -> [[a]]
-modifyAt2d f x y = modifyAt (modifyAt f x) y
+modifyAt2d f x y = modifyAt (modifyAt f x) y {- HLINT ignore "Eta reduce" -}
+
+initialFabric :: Fabric
+initialFabric = replicate 1000 (replicate 1000 0)
 
 applyClaim :: Fabric -> Claim -> Fabric
-applyClaim fabric Claim { x, y, w, h } = foldl (&) fabric modifications
-  where
-    xs = [x .. (x + (w - 1))]
-    ys = [y .. (y + (h - 1))]
-    modifications = modifyAt2d (+1) <$> xs <*> ys
+applyClaim fabric Claim { _x, _y, _width, _height } =
+  foldl (&) fabric (modifyAt2d (+ 1) <$> xs <*> ys)
+  where xs = [_x .. ((_x - 1) + _width)]
+        ys = [_y .. ((_y - 1) + _height)]
 
 applyClaims :: Fabric -> [Claim] -> Fabric
-applyClaims fabric = foldl applyClaim fabric
+applyClaims = foldl applyClaim
 
-countOverlapping :: Fabric -> Int
-countOverlapping = length . filter (> 1) . concat
+overlapping :: Fabric -> Int
+overlapping = length . filter (>= 2) . concat
+
+solve :: [Claim] -> Int
+solve = overlapping . applyClaims initialFabric
 
 main :: IO ()
-main = print
-     . countOverlapping
-     . applyClaims initialFabric
-     . fmap parse
-     . lines
-   =<< getContents
-
-{- HLINT ignore "Eta reduce" -}
+main = print . solve . parse =<< getContents
