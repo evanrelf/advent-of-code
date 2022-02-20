18f4e3ee79a41a346e76d64759387af5666d8293
Evan Relf <evan@evanrelf.com>
2022-02-19 23:32:18 -0800

    Stop allocating in Zig solution for 2021-1-1

--- zig/2021/1-1.zig
+++ zig/2021/1-1.zig
@@ -1,36 +1,30 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
-
     const stdin = std.io.getStdIn();
     defer stdin.close();
+    const stdin_reader = stdin.reader();
 
-    var depths = std.ArrayList(u16).init(allocator);
-    defer depths.deinit();
-
-    while (true) {
-        const line = try stdin.reader().readUntilDelimiterOrEofAlloc(allocator, '\n', 5);
-        if (line) |l| {
-            const depth = try std.fmt.parseUnsigned(u16, l, 10);
-            try depths.append(depth);
-        } else {
-            break;
-        }
-    }
+    var buffer: [5]u8 = undefined;
 
     var increases_count: u16 = 0;
     var previous_depth: ?u16 = null;
 
-    for (depths.items) |current_depth| {
-        if (previous_depth) |prev| {
-            if (current_depth > prev) {
-                increases_count += 1;
+    while (true) {
+        const line = try stdin_reader.readUntilDelimiterOrEof(&buffer, '\n');
+        if (line) |l| {
+            const current_depth = try std.fmt.parseUnsigned(u16, l, 10);
+
+            if (previous_depth) |prev| {
+                if (current_depth > prev) {
+                    increases_count += 1;
+                }
             }
+
+            previous_depth = current_depth;
+        } else {
+            break;
         }
-        previous_depth = current_depth;
     }
 
     std.debug.print("Increases: {d}\n", .{increases_count});
