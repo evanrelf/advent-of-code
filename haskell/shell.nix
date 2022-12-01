2701550c642f084c6800f730d9d4a800befa1bc3
Evan Relf <evan@evanrelf.com>
2022-11-30 21:16:59 -0800

    Use boring Nix shell

--- haskell/shell.nix
+++ haskell/shell.nix
@@ -1,1 +1,21 @@
-(import ./.).shell
+let
+  pkgs = import <nixpkgs> { };
+
+in
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.cabal-install
+    (pkgs.ghc.withPackages (p: [
+      p.bytestring
+      p.containers
+      p.generic-lens
+      p.lens
+      p.megaparsec
+      p.relude
+      p.streamly
+      p.text
+      p.vector
+    ]))
+    pkgs.ghcid
+  ];
+}
