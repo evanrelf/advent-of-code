2492d1233bdb7c82d82e3f23174a5509d1a8d3ad
Evan Relf <evan@evanrelf.com>
2022-11-30 21:59:52 -0800

    Finish 2022 day 1 part 1

---
+++ haskell/2022/1-1.hs
@@ -0,0 +1,25 @@
+module Main (main) where
+
+import qualified Data.List as List
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+import qualified Relude.Unsafe as Unsafe
+
+main :: IO ()
+main = do
+  input <- Text.getContents
+  print $ solve (parse input)
+
+parse :: Text -> [[Word]]
+parse input =
+  input
+  & Text.strip
+  & Text.splitOn "\n\n"
+  & fmap Text.lines
+  & fmap (fmap (toString >>> Unsafe.read @Word))
+
+solve :: [[Word]] -> Word
+solve inventories =
+  inventories
+  & fmap sum
+  & List.maximum
