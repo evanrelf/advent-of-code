5ead14610f18e6884328684caceefeabcc83ae7b
Evan Relf <evan@evanrelf.com>
2023-12-03 20:56:49 -0800

    Solve 2032 day 2 part 2

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ fn main() -> anyhow::Result<ExitCode> {
         (2023, 1, 1) => year_2023::day_01_1::solve(&get_input()?)?,
         (2023, 1, 2) => year_2023::day_01_2::solve(&get_input()?)?,
         (2023, 2, 1) => year_2023::day_02_1::solve(&get_input()?)?,
+        (2023, 2, 2) => year_2023::day_02_2::solve(&get_input()?)?,
         (year, day, part) => {
             eprintln!("No solution for {year} day {day} part {part}");
             return Ok(ExitCode::FAILURE);
