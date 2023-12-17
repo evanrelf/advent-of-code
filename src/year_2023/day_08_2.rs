e3e46bb417ba470581a3126178b2f285f32d420c
Evan Relf <evan@evanrelf.com>
2023-12-17 14:02:54 -0800

    wip: Solve 2032 day 8 part 2

--- src/year_2023/day_08_2.rs
+++ src/year_2023/day_08_2.rs
@@ -1,5 +1,13 @@
-use anyhow::{anyhow, Context as _};
-use std::{collections::HashMap, str::FromStr};
+use anyhow::anyhow;
+use std::{
+    collections::HashMap,
+    str::FromStr,
+    sync::{
+        atomic::{AtomicU8, Ordering},
+        Arc, Barrier,
+    },
+    thread,
+};
 use winnow::{
     ascii::line_ending,
     combinator::{dispatch, fail, repeat, separated, success},
@@ -11,33 +19,83 @@ crate::aoc!();
 
 pub fn solve(input: &str) -> anyhow::Result<usize> {
     let network = input.parse::<Network>()?;
-    unreachable!()
+
+    let starts = network.paths.keys().filter(|Node(node)| node[2] == b'A');
+
+    let thread_count = starts.clone().count();
+
+    assert!(thread_count < 20, "too many threads");
+
+    let barrier = Arc::new(Barrier::new(thread_count + 1));
+
+    let done_counter = AtomicU8::new(0);
+
+    thread::scope(|scope| {
+        for start in starts {
+            scope.spawn(|| {
+                for Node(node) in path_iter(*start, &network) {
+                    if node[2] == b'Z' {
+                        done_counter.fetch_add(1, Ordering::SeqCst);
+                    }
+                    barrier.wait();
+
+                    let done_count = done_counter.load(Ordering::SeqCst);
+                    let done = usize::from(done_count) == thread_count;
+                    barrier.wait();
+
+                    // Main thread resetting count
+                    barrier.wait();
+
+                    if done {
+                        return;
+                    }
+                }
+            });
+        }
+
+        let mut step = 0;
+
+        loop {
+            // Worker threads adding to count
+            barrier.wait();
+
+            let done_count = done_counter.load(Ordering::SeqCst);
+            let done = usize::from(done_count) == thread_count;
+            barrier.wait();
+
+            println!("step: {step}, done: {done_count}/{thread_count}");
+            done_counter.store(0, Ordering::SeqCst);
+            barrier.wait();
+
+            if done {
+                return Ok(step);
+            }
+            step += 1;
+        }
+    })
 }
 
-fn path_iter(start: Node, network: &Network) -> impl Iterator<Item = (Step, Node)> {
-    let mut here = start;
-
+fn path_iter(start: Node, network: &Network) -> impl Iterator<Item = Node> + '_ {
     network
         .instructions
         .iter()
-        .fold(start, |node, instruction| {
+        .cycle()
+        .scan(start, move |here, instruction| {
+            let node = *here;
+
             let (left, right) = network
                 .paths
-                .get(&here)
-                .expect("Paths in puzzle input are valid");
+                .get(&node)
+                .expect("Puzzle input has valid paths");
 
-            let node = match instruction {
+            *here = match instruction {
                 Instruction::Left => *left,
                 Instruction::Right => *right,
-            }
+            };
 
-            (Step(step), node)
+            Some(node)
         })
-        .take_while(move |(step, node)| *step == Step(0) && *node != start)
 }
-
-#[derive(Debug, PartialEq)]
-struct Step(usize);
 
 struct Network {
     instructions: Vec<Instruction>,
@@ -98,8 +156,8 @@ fn path(input: &mut &str) -> PResult<Path> {
     Ok(Path { start, left, right })
 }
 
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
-struct Node([char; 3]);
+#[derive(Clone, Copy, Debug, Hash, Eq, PartialEq)]
+struct Node([u8; 3]);
 
 impl FromStr for Node {
     type Err = anyhow::Error;
@@ -110,14 +168,19 @@ impl FromStr for Node {
 }
 
 fn node(input: &mut &str) -> PResult<Node> {
-    let letters: Vec<_> = repeat(3, one_of(('A'..='Z', '0'..='9'))).parse_next(input)?;
-    Ok(Node([letters[0], letters[1], letters[2]]))
+    let chars: Vec<_> = repeat(3, one_of((b'A'..=b'Z', b'0'..=b'9'))).parse_next(input)?;
+    Ok(Node([
+        u8::try_from(chars[0]).expect("Parsed chars are ASCII"),
+        u8::try_from(chars[1]).expect("Parsed chars are ASCII"),
+        u8::try_from(chars[2]).expect("Parsed chars are ASCII"),
+    ]))
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use indoc::indoc;
+    use std::iter::zip;
 
     const SAMPLE: &str = indoc! {"
         LR
@@ -134,29 +197,25 @@ mod tests {
 
     #[test]
     fn test_sample() {
-        assert_eq!(solve(SAMPLE).unwrap(), 2);
+        assert_eq!(solve(SAMPLE).unwrap(), 6);
     }
 
     #[test]
     fn test_path_iter() {
         let network = SAMPLE.parse::<Network>().unwrap();
 
-        let start = Node(['1', '1', 'A']);
-        let expected: Vec<(Step, Node)> = vec!["11A", "11B", "11Z", "11B", "11Z", "11B", "11Z"]
+        let start = Node([b'1', b'1', b'A']);
+        let expected = vec!["11A", "11B", "11Z", "11B", "11Z", "11B", "11Z"]
             .into_iter()
-            .enumerate()
-            .map(|(step, node)| (Step(step), node.parse().unwrap()))
-            .collect();
-        let actual = path_iter(start, &network).take(7).collect::<Vec<_>>();
-        assert_eq!(expected, actual);
+            .map(|node| node.parse::<Node>().unwrap());
+        let actual = path_iter(start, &network);
+        assert!(zip(expected, actual).all(|(e, a)| e == a));
 
-        let start = Node(['2', '2', 'A']);
-        let expected: Vec<(Step, Node)> = vec!["22A", "22B", "22C", "22Z", "22B", "22C", "22Z"]
+        let start = Node([b'2', b'2', b'A']);
+        let expected = vec!["22A", "22B", "22C", "22Z", "22B", "22C", "22Z"]
             .into_iter()
-            .enumerate()
-            .map(|(step, node)| (Step(step), node.parse().unwrap()))
-            .collect();
-        let actual = path_iter(start, &network).take(7).collect::<Vec<_>>();
-        assert_eq!(expected, actual);
+            .map(|node| node.parse::<Node>().unwrap());
+        let actual = path_iter(start, &network);
+        assert!(zip(expected, actual).all(|(e, a)| e == a));
     }
 }
