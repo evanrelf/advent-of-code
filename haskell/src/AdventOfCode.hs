5222a4fb82867cdb4636f1bf6fd08ba6acc7f9f6
Evan Relf <evan@evanrelf.com>
2024-06-14 20:25:53 -0700

    Set up test suite

--- haskell/src/AdventOfCode.hs
+++ haskell/src/AdventOfCode.hs
@@ -1,13 +1,33 @@
 {-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE QuasiQuotes #-}
 
-module AdventOfCode (main) where
+module AdventOfCode (main, test) where
 
+import AdventOfCode.Year2023.Day01.Part1 qualified
 import Control.Exception (throwIO)
 import Data.ByteString qualified as ByteString
 import Data.String.Interpolate (i)
 import Data.Text.IO qualified as Text
 import Options.Applicative
+import Test.Tasty qualified as Tasty
+
+main :: IO ()
+main = do
+  options <- getOptions
+  solve <- case (options.year, options.day, options.part) of
+    (y, d, p) -> do
+      Text.hPutStrLn stderr [i|No solution for year #{y} day #{d} part #{p}|]
+      exitFailure
+  inputBytes <- ByteString.getContents
+  input <- either throwIO pure (decodeUtf8' inputBytes)
+  case solve input of
+    Left err -> die err
+    Right (Showable solution) -> print solution
+
+test :: IO ()
+test = Tasty.defaultMain . Tasty.testGroup "tests" $
+  [ Tasty.testGroup "2023 01 1" AdventOfCode.Year2023.Day01.Part1.tests
+  ]
 
 data Options = Options
   { year :: Word16
@@ -27,19 +47,6 @@ getOptions = do
   let parserPrefs = prefs mempty
   let parserInfo = info (helper <*> parseOptions) mempty
   customExecParser parserPrefs parserInfo
-
-main :: IO ()
-main = do
-  options <- getOptions
-  solve <- case (options.year, options.day, options.part) of
-    (y, d, p) -> do
-      Text.hPutStrLn stderr [i|No solution for year #{y} day #{d} part #{p}|]
-      exitFailure
-  inputBytes <- ByteString.getContents
-  input <- either throwIO pure (decodeUtf8' inputBytes)
-  case solve input of
-    Left err -> die err
-    Right (Showable solution) -> print solution
 
 data Showable where
   Showable :: Show a => a -> Showable
---
+++ haskell/src/Test.hs
@@ -0,0 +1,6 @@
+module Main (main) where
+
+import AdventOfCode qualified
+
+main :: IO ()
+main = AdventOfCode.test
