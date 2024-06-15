5cb07a251bbac11014978b25e9c79ab73479a3af
Evan Relf <evan@evanrelf.com>
2024-06-14 21:07:02 -0700

    Add failing test

--- haskell/src/AdventOfCode/Year2023/Day01/Part1.hs
+++ haskell/src/AdventOfCode/Year2023/Day01/Part1.hs
@@ -1,9 +1,23 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module AdventOfCode.Year2023.Day01.Part1 (solve, tests) where
 
+import Data.String.Interpolate (__i)
 import Test.Tasty
+import Test.Tasty.HUnit
 
-solve :: Text -> Either e Text
-solve input = Right input
+solve :: Text -> Either String Word
+solve _ = Left "todo"
 
 tests :: [TestTree]
-tests = []
+tests =
+  [ testCase "example input" $ solve exampleInput @?= Right 142
+  ]
+
+exampleInput :: Text
+exampleInput = [__i|
+  1abc2
+  pqr3stu8vwx
+  a1b2c3d4e5f
+  treb7uchet
+|]
