5425aa6aafa58d4d51a471edff5e065affbed183
Evan Relf <evan@evanrelf.com>
2024-06-14 21:18:27 -0700

    Initial solution for 2023 day 01 part 1

--- haskell/src/AdventOfCode/Year2023/Day01/Part1.hs
+++ haskell/src/AdventOfCode/Year2023/Day01/Part1.hs
@@ -2,16 +2,28 @@
 
 module AdventOfCode.Year2023.Day01.Part1 (solve, tests) where
 
+import Data.Char (isDigit)
 import Data.String.Interpolate (__i)
+import Data.Text qualified as Text
+import Relude.Extra.Bifunctor (bimapBoth)
+import Relude.Unsafe (read)
 import Test.Tasty
 import Test.Tasty.HUnit
 
-solve :: Text -> Either String Word
-solve _ = Left "todo"
+solve :: Text -> Word
+solve input =
+  input
+     &  Text.lines
+    <&> Text.filter isDigit
+    <&> Text.head &&& Text.last
+    <&> bimapBoth (one >>> read @Word)
+    <&> first (* 10)
+    <&> uncurry (+)
+     &  sum
 
 tests :: [TestTree]
 tests =
-  [ testCase "example input" $ solve exampleInput @?= Right 142
+  [ testCase "example input" $ solve exampleInput @?= 142
   ]
 
 exampleInput :: Text
